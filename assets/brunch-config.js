3e319e80781f16adaebe5e13a4a9fbe0291ea472
Edwin Bühler <edbuehler@icloud.com>
2018-02-20 09:22:59 +0100

    initial commit

--- assets/brunch-config.js
+++ assets/brunch-config.js
@@ -1,15 +1,36 @@
 exports.config = {
+  // See http://brunch.io/#documentation for docs.
   files: {
     javascripts: { joinTo: "js/app.js" },
+    // To use a separate vendor.js bundle, specify two files path
+      // http://brunch.io/docs/config#-files-
+      // joinTo: {
+      //   "js/app.js": /^js/,
+      //   "js/vendor.js": /^(?!js)/
+      // }
+      //
+      // To change the order of concatenation of files, explicitly mention here
+      // order: {
+      //   before: [
+      //     "vendor/js/jquery-2.1.1.js",
+      //     "vendor/js/bootstrap.min.js"
+      //   ]
+      // }
     stylesheets: { joinTo: "css/app.css" },
     templates: { joinTo: "js/app.js" }
   },
-  conventions: { assets: /^(static)/ },
+  conventions: { 
+    // This option sets where we should place non-css and non-js assets in.
+    // By default, we set this to "/assets/static". Files in this directory
+    // will be copied to `paths.public`, which is "priv/static" by default.
+    assets: /^(static)/ },
+    // Phoenix paths configuration
   paths: {
     watched: ["static", "css", "js", "vendor", "elm"],
     public: "../priv/static"
   },
-  plugins: {
+  // Configure your plugins
+  plugins: {// Do not use ES6 compiler in vendor code
     babel: { ignore: [/vendor/] },
     elmBrunch: {
       mainModules: ["elm/Main.elm"],
--- lib/edwin_app_web/router.ex
+++ lib/edwin_app_web/router.ex
@@ -17,6 +17,7 @@ defmodule EdwinAppWeb.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    get "/impressum", PageController, :impressum
   end
 
   # Other scopes may use custom stacks.
