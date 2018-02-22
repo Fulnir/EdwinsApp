ff00234ed5188c6371b673c61f40c02ec4bcfa60
Edwin Bühler <edbuehler@icloud.com>
2018-02-22 20:10:52 +0100

    wieder mit elm

--- assets/brunch-config.js
+++ assets/brunch-config.js
@@ -1,10 +1,8 @@
 exports.config = {
   // See http://brunch.io/#documentation for docs.
   files: {
-    javascripts: {
-      joinTo: "js/app.js"
-
-      // To use a separate vendor.js bundle, specify two files path
+    javascripts: { joinTo: "js/app.js" },
+    // To use a separate vendor.js bundle, specify two files path
       // http://brunch.io/docs/config#-files-
       // joinTo: {
       //   "js/app.js": /^js/,
@@ -18,45 +16,31 @@ exports.config = {
       //     "vendor/js/bootstrap.min.js"
       //   ]
       // }
-    },
-    stylesheets: {
-      joinTo: "css/app.css"
-    },
-    templates: {
-      joinTo: "js/app.js"
-    }
+    stylesheets: { joinTo: "css/app.css" },
+    templates: { joinTo: "js/app.js" }
   },
-
-  conventions: {
+  conventions: { 
     // This option sets where we should place non-css and non-js assets in.
     // By default, we set this to "/assets/static". Files in this directory
     // will be copied to `paths.public`, which is "priv/static" by default.
-    assets: /^(static)/
-  },
-
-  // Phoenix paths configuration
+    assets: /^(static)/ },
+    // Phoenix paths configuration
   paths: {
-    // Dependencies and current project directories to watch
-    watched: ["static", "css", "js", "vendor"],
-    // Where to compile files to
+    watched: ["static", "css", "js", "vendor", "elm"],
     public: "../priv/static"
   },
-
   // Configure your plugins
-  plugins: {
-    babel: {
-      // Do not use ES6 compiler in vendor code
-      ignore: [/vendor/]
+  plugins: {// Do not use ES6 compiler in vendor code
+    babel: { ignore: [/vendor/] },
+    elmBrunch: {
+      mainModules: ["elm/Main.elm"],
+      makeParameters: ["--debug"],
+      outputFile: "elm.js",
+      outputFolder: "../assets/js"
     }
   },
-
   modules: {
-    autoRequire: {
-      "js/app.js": ["js/app"]
-    }
+    autoRequire: { "js/app.js": ["js/app"] }
   },
-
-  npm: {
-    enabled: true
-  }
+  npm: { enabled: true }
 };
